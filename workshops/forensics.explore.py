4ad7e54a0af597e77ede2535dea466723e40487b
Akond Rahman <akond.rahman.buet@gmail.com>
2020-09-05 16:25:47 -0500

    forensics workshop

---
+++ workshops/forensics.explore.py
@@ -0,0 +1,36 @@
+'''
+Akond Rahman 
+Sep 04, 2020 
+Friday 
+'''
+import logging 
+LOGFORMAT = '%(asctime)s:::%(levelname)s:::%(message)s'
+# logging.basicConfig(filename='practice.log') 
+
+# logging.basicConfig(format='%(asctime)s %(message)s') ## this will not work to get time stamp 
+
+def doLogging():
+    # logging.basicConfig(filename='practice.log', level=logging.DEBUG, format=LOGFORMAT) :: working 
+    logging.info('Before variables ... datetime not enabled ' )
+    fName  = 'init.sls' 
+    logging.debug('After variable ... datetime not enabled ' +  fName) 
+    logging.warning('Now we have date time')   
+
+def getLogging():
+    ## logging with log object 
+    loggerObj = logging.getLogger('EXAMPLE')
+    # print(loggerObj.__dict__) 
+    loggerObj.setLevel(logging.DEBUG) 
+
+    file_handler = logging.FileHandler('practice.log') 
+    formatter = logging.Formatter(LOGFORMAT) 
+    file_handler.setFormatter(formatter) 
+    loggerObj.addHandler(file_handler) 
+
+    loggerObj.debug('Got the logger (debug) ... ')
+    loggerObj.info('Got the logger (info) ... ')    
+ 
+
+if __name__=='__main__':
+    # doLogging() 
+    getLogging() 
