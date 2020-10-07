b81f000af851ba6822cf44605a56ecd792c127e0
Akond Rahman <akond.rahman.buet@gmail.com>
2020-10-07 13:34:24 -0500

    recursion done

--- workshops/py.traverse.py
+++ workshops/py.traverse.py
@@ -7,8 +7,9 @@ import ast
 import os 
 
 
-def getBinOpDetails(assiTarget, assiValue ): 
-    lhs_var, rhs_var = '', ''
+def getBinOpDetails(assiTarget, assiValue, element_type = 'SINGLE_ASSIGNMENT' ): 
+    lhs_var, rhs_var = '', '' 
+    var_assignment_list = []
 
     for target in assiTarget:
         if isinstance(target, ast.Name):
@@ -22,10 +23,11 @@ def getBinOpDetails(assiTarget, assiValue ):
                 # print("Operand:" + op_.id ) 
     elif isinstance(assiValue, ast.Num):
         rhs_var = assiValue.n 
-    var_assignment_list = [( lhs_var, rhs_var   ) ]
+    if len(lhs_var) > 0:
+        var_assignment_list = [( lhs_var, rhs_var , element_type  ) ]
     return var_assignment_list
 
-def getTupAssiDetails(assiTargets, assiValue ): 
+def getTupAssiDetails(assiTargets, assiValue, element_type = 'TUPLE_ASSIGNMENT' ): 
     # print('INSIDE TUPLE')
     var_assignment_list = []
     # print(assiTargets, assiValue)
@@ -47,55 +49,125 @@ def getTupAssiDetails(assiTargets, assiValue ):
                     var_value =  value.s 
                 if isinstance(name, ast.Name):
                     var_name = name.id 
-                var_assignment_list.append( (var_name, var_value) )
+                var_assignment_list.append( (var_name, var_value, element_type) )
     return var_assignment_list     
 
-def getVariables(path2program):
+def getCommonAssiDetails(assignDict, elemType):
+    assignTargets, assignValue = assignDict['targets'], assignDict['value']
+    var_details_bin  = getBinOpDetails( assignTargets, assignValue , elemType ) 
+    var_details_tup  = getTupAssiDetails( assignTargets, assignValue , elemType ) 
+    return var_details_bin, var_details_tup 
+
+def getVariables(tree_, elemTypeParam):
+    final_list  = [] 
+    for stmt_ in tree_.body:
+        for node_ in ast.walk(stmt_):
+            assignDict = node_.__dict__
+            if isinstance(node_, ast.Assign)  :
+                bin_res, tup_res = getCommonAssiDetails( assignDict, elemTypeParam )
+                if len(bin_res) > 0:
+                    final_list = final_list + bin_res
+                if len(tup_res) > 0: 
+                    final_list = final_list + tup_res 
+            elif isinstance(node_, ast.AugAssign):
+                temp = [] 
+                assignTarget, assignValue = assignDict['target'], assignDict['value']
+                temp.append( assignTarget )
+                var_details = getBinOpDetails( temp , assignValue ) 
+                final_list = final_list + var_details 
+    return final_list 
+
+def getFunctionAssignments(full_tree):
+    call_list = []
+    for stmt_ in full_tree.body:
+        for node_ in ast.walk(stmt_):
+            if isinstance(node_, ast.Assign):
+                lhs = ''
+                assign_dict = node_.__dict__
+                targets, value  =  assign_dict['targets'], assign_dict['value']
+                if isinstance(value, ast.Call):
+                    funcDict = value.__dict__ 
+                    funcName, funcArgs, funcLineNo =  funcDict['func'], funcDict['args'], funcDict['lineno'] 
+                    for target in targets:
+                        if( isinstance(target, ast.Name) ):
+                            lhs = target.id 
+                    for x_ in funcArgs:
+                        if( isinstance(x_, ast.Name ) ) and ( isinstance(funcName, ast.Name ) ):
+                            arg_index =  1
+                            call_list.append( ( lhs, funcName.id, x_.id, 'FUNC_CALL_ARG:' + str(arg_index) )  )
+                            arg_index = arg_index + 1
+    return call_list 
+
+def giveVarsInIf(body_):
+    var_list = [] 
+    assign_dict = body_.__dict__ 
+    # print(assign_dict)
+    if (isinstance( body_, ast.IfExp )  or isinstance( body_, ast.If )):
+        if 'body' in assign_dict:
+            ifbody  = assign_dict['body'] 
+            for bod_elem in ifbody:
+                if isinstance(bod_elem, ast.Assign ):
+                    assignDict = bod_elem.__dict__ 
+                    bin_res, tup_res = getCommonAssiDetails( assignDict, 'FUNC_VAR_ASSIGNMENT' )
+                    if len(bin_res) > 0:
+                        var_list = var_list + bin_res
+                    if len(tup_res) > 0: 
+                        var_list = var_list + tup_res 
+        elif 'orelse' in assign_dict:
+            orlesebody  = assign_dict['orelse'] 
+            # print(orlesebody) 
+            var_list =  giveVarsInIf( orlesebody ) 
+        return var_list 
+    else: 
+        return var_list 
+
+    
+
+
+def getFunctionDefinitions(path2program):
+    call_sequence_ls = [] 
+    func_var_list = []
     if os.path.exists(path2program):
         full_tree = ast.parse( open( path2program  ).read() )
         # print( ast.dump( full_tree )  )
         for stmt_ in full_tree.body:
             for node_ in ast.walk(stmt_):
-                if isinstance(node_, ast.Assign)  :
-                    assignDict = node_.__dict__
-                    assignTargets, assignValue = assignDict['targets'], assignDict['value']
-                    var_details_bin  = getBinOpDetails( assignTargets, assignValue ) 
-                    # print(assignDict) 
-                    var_details_tup  = getTupAssiDetails( assignTargets, assignValue ) 
-                    # print(var_details_tup) 
-                elif isinstance(node_, ast.AugAssign):
-                    assignDict = node_.__dict__
-                    temp = [] 
-                    # print(assignDict)
-                    assignTarget, assignValue = assignDict['target'], assignDict['value']
-                    temp.append( assignTarget )
-                    var_details = getBinOpDetails( temp , assignValue ) 
-                    # print( var_details )
+                if isinstance(node_, ast.FunctionDef):
+                    func_def_dict = node_.__dict__
+                    # print(func_def_dict) 
+                    func_name, func_args, func_body_parts = func_def_dict['name'], func_def_dict['args'], func_def_dict['body']
+                    if(isinstance( func_args, ast.arguments )):
+                        arg_index = 1
+                        args = func_args.__dict__['args']
+                        for arg_ in args:
+                            call_sequence_ls.append( (func_name, arg_.__dict__['arg'], 'FUNC_DEFI:' + str(arg_index) ) )
+                            arg_index = arg_index + 1
+                    # print(func_body_parts)
+                    for body_ in func_body_parts:
+                        assign_dict = body_.__dict__ 
+                        if (isinstance( body_, ast.Assign )):
+                            func_var_list = func_var_list + getBinOpDetails( assign_dict['targets'], assign_dict['value'], 'FUNC_SINGLE_ASSIGNMENT' )
+                        elif (isinstance( body_, ast.IfExp )  or isinstance( body_, ast.If )):
+                            func_var_list = func_var_list + giveVarsInIf(  body_ )
 
 
+                 
+    return call_sequence_ls, func_var_list 
 
-
-def getFunctionAssignments(path2program):
-    if os.path.exists(path2program):
-        full_tree = ast.parse( open( path2program  ).read() )
-        # print( ast.dump( full_tree )  )
-        for stmt_ in full_tree.body:
-            for node_ in ast.walk(stmt_):
-                if isinstance(node_, ast.Assign):
-                    lhs = ''
-                    assign_dict = node_.__dict__
-                    targets, value  =  assign_dict['targets'], assign_dict['value']
-                    if isinstance(value, ast.Call):
-                        funcDict = value.__dict__ 
-                        funcName, funcArgs, funcLineNo =  funcDict['func'], funcDict['args'], funcDict['lineno'] 
-                        for target in targets:
-                            if( isinstance(target, ast.Name) ):
-                                lhs = target.id 
-                        for x_ in funcArgs:
-                            if( isinstance(x_, ast.Name ) ) and ( isinstance(funcName, ast.Name ) ):
-                                print( lhs, "=", funcName.id,  x_.id  ) 
 
 if __name__=='__main__':
     input_program = 'fuzz.py' 
-    getVariables(input_program) 
-    getFunctionAssignments( input_program ) 
+    full_tree = None 
+    if os.path.exists(input_program):
+        full_tree = ast.parse( open( input_program  ).read() )    
+        print('*'*25)
+        fullVarList = getVariables(full_tree, 'VAR_ASSIGNMENT')  
+        print(fullVarList) 
+        print('*'*25)
+        call_list = getFunctionAssignments( full_tree ) 
+        print(call_list) 
+        print('*'*25)
+        funcDefList, funcvarList = getFunctionDefinitions(input_program) 
+        print(funcDefList)
+        print(funcvarList)
+        print('*'*25)
