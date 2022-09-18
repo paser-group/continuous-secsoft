8918f97d4c5476ea1dc6f2022abd647baf8a6c2e
Akond Rahman <akond.rahman.buet@gmail.com>
2022-09-18 14:51:47 -0500

    codacy test

---
+++ software-quality-assurance/workshops/simple.py
@@ -0,0 +1,13 @@
+def simpleDiv(a, b):
+    return a/b 
+
+
+if __name__=='__main__': 
+   print('Welcome to the simple division calculator!')
+   print('*'*50)
+   a=input('Provide first input:')
+   print('Thanks') 
+   b=input('Provide second input:')
+   a, b = int(a), int(b)
+   res=simpleDiv(a, b) 
+   print('The answer is:', res)
