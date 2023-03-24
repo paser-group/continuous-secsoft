97f7cbb1f5a000d0e4e62cb39408935636b44c63
Akond Rahman <akond.rahman.buet@gmail.com>
2023-03-24 10:38:58 -0500

    workshop 8

--- software-quality-assurance/workshops/workshop7/simple.py
+++ software-quality-assurance/workshops/workshop7/simple.py
@@ -7,10 +7,15 @@ def simpleDiv(a, b):
 
 if __name__=='__main__': 
    print('Welcome to the simple division calculator!')
+   logObj = logging_example.giveMeLoggingObject()
+   logObj.info('Executing simple.py') 
    print('*'*50)
    a=input('Provide first input:')
+   logObj.info('Collected first input') 
    print('Thanks') 
    b=input('Provide second input:')
+   logObj.info('Collected second input') 
    a, b = int(a), int(b)
    res=simpleDiv(a, b) 
    print('The answer is:', res)
+   logObj.info('Finished executing simple.py') 
