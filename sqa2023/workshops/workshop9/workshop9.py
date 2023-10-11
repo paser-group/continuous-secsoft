7288d401cb6e5ca78e44d464907d8f8ab0309ba5
Akond Rahman <akond.rahman.buet@gmail.com>
2023-10-11 07:42:29 -0500

    workshop 8 and 9

---
+++ sqa2023/workshops/workshop9/workshop9.py
@@ -0,0 +1,19 @@
+'''
+Author: Akond Rahman 
+Code needed for Workshop 9
+'''
+
+import random 
+
+def divide(v1, v2):
+   return v1 / v2 
+
+def fuzzValues(val1, val2):
+   res = divide(val1, val2)
+   return res  
+
+def simpleFuzzer(): 
+    pass 
+
+if __name__=='__main__':
+    simpleFuzzer()
