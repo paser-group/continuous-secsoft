3de1bcaf920e1b4141e14a43cee1d4d19d97318d
Akond Rahman <akond.rahman.buet@gmail.com>
2024-03-11 11:10:31 -0500

    workshop 9

---
+++ sqa-spring2024/workshops/workshop9-forensics/simple.py
@@ -0,0 +1,13 @@
+'''
+Akond Rahman
+Simple Python file to demonstrate logging 
+'''
+import myLogger
+
+
+def simpleDiv(a, b):
+    return a/b 
+
+
+if __name__=='__main__': 
+   print('Welcome to the simple division calculator!')
