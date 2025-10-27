8e08a4af5e125f541fb6e013f72075f04084e8ef
Akond Rahman <akond.rahman.buet@gmail.com>
2025-10-27 06:02:35 -0500

    w10

---
+++ fall25-sqa/workshops/w10-fuzz/w10.py
@@ -0,0 +1,16 @@
+'''
+Author: Akond Rahman 
+'''
+
+import random 
+
+def divide(v1, v2):
+    return v1/ v2 
+
+def simpleFuzzer(): 
+    # TODO 
+    pass 
+
+
+if __name__=='__main__':
+    simpleFuzzer()
